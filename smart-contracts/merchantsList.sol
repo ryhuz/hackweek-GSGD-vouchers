6e9846e8b79765b4b88db8b39a52eb0d824686b0
ryhuz <shawn.wee90@hotmail.com>
2023-02-02 13:44:25 +0800

    [SW] add merchants list contract

---
+++ smart-contracts/merchantsList.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.6;
+
+import "./node_modules/@openzeppelin/contracts/access/Ownable.sol";
+
+contract MerchantsList is Ownable {
+    struct Merchant {
+        string name;
+        bool onboarded;
+    }
+    mapping (address => Merchant) merchantMap;
+
+    function onboardMerchant (address merchantAddress, string calldata name) public onlyOwner {
+        merchantMap[merchantAddress] = Merchant(name, true);
+    }
+
+    function merchantExists (address _address) public view returns(bool) {
+        return merchantMap[_address].onboarded;
+    }
+}
