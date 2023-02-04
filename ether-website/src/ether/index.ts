0888d70b89ba88c9274fe97dac79d3a62fef06bd
choo cw <chenwei.choo@ufinity.com>
2023-02-04 16:21:49 +0800

    [CCW] cleanup package.json and remove pk

--- ether-website/src/ether/index.ts
+++ ether-website/src/ether/index.ts
@@ -2,13 +2,13 @@ import { ethers, Wallet } from "ethers";
 
 class EtherHelper {
   //ganache account privat key
-  private privateKey =
-    "0xb2cd0b01727fa85ac21360bd578ead13d5f5eebd88d6aa2bb80931eaeabd73e3";
+  private privateKey = "";
   private blockChainUrl = "http://0.0.0.0:8545";
 
   private wallet: Wallet;
 
   constructor() {
+    //TODO: take in pk form different accounts
     const provider = new ethers.JsonRpcProvider(this.blockChainUrl);
     this.wallet = new ethers.Wallet(this.privateKey, provider);
   }
