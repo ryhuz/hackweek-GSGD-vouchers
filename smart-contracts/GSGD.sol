eddd8a4ab57cc36b436d80132af2621b403e270f
shawn wee <shawn.wee@ufinity.com>
2023-02-06 17:36:37 +0800

    [SW] added access control to GSGD

--- smart-contracts/GSGD.sol
+++ smart-contracts/GSGD.sol
@@ -4,12 +4,18 @@ pragma solidity ^0.8.6;
 import "./node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "./node_modules/@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "./node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "./node_modules/@openzeppelin/contracts/access/AccessControl.sol";
 import "./utils/enums.sol";
 
-contract GSGD is ERC20, ERC20Burnable, Ownable {
+contract GSGD is ERC20, ERC20Burnable, Ownable, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
     uint totalRedeemed = 0;
     mapping (Enums.Denominations => uint) valueMap;
-    constructor() ERC20("GSGD", "GSGD") {
+    constructor() ERC20("SGD Tokens", "GSGD") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+
         valueMap[Enums.Denominations.TWO] = 2;
         valueMap[Enums.Denominations.FIVE] = 5;
         valueMap[Enums.Denominations.TEN] = 10;
@@ -19,7 +25,11 @@ contract GSGD is ERC20, ERC20Burnable, Ownable {
         return 0;
     }
 
-    function issueToMerchant(address merchantAddress, Enums.Denominations amount) external {
+    function setVoucherAddressAsMinter (address voucherAddress) public onlyOwner {
+        _grantRole(MINTER_ROLE, voucherAddress);
+    }
+
+    function issueToMerchant(address merchantAddress, Enums.Denominations amount) external onlyRole(MINTER_ROLE) {
         _mint(merchantAddress, valueMap[amount]);
     }
 
