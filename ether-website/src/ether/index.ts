09a88706a2765521784e242bbbd4ee776ac9aca2
ryhuz <shawn.wee90@hotmail.com>
2023-02-09 15:44:30 +0800

    [SW] use blockchain url from keys

--- ether-website/src/ether/index.ts
+++ ether-website/src/ether/index.ts
@@ -1,46 +1,42 @@
 import { Contract, ContractInterface, ethers, Wallet, Signer } from "ethers";
+import { _blockChainUrl } from "../keys";
 
 export class EtherHelper {
-  private provider: ethers.providers.JsonRpcProvider;
+	private provider: ethers.providers.JsonRpcProvider =
+		new ethers.providers.JsonRpcProvider(_blockChainUrl);
 
-  constructor(blockChainUrl?: string) {
-    //currently set to this
-    blockChainUrl = "http://0.0.0.0:8545";
-    this.provider = new ethers.providers.JsonRpcProvider(blockChainUrl);
-  }
+	public getProvider() {
+		if (this.provider) {
+			return this.provider;
+		}
+		console.log("Provider not provided, please set a provider");
+		return null;
+	}
 
-  public getProvider() {
-    if (this.provider) {
-      return this.provider;
-    }
-    console.log("Provider not provided, please set a provider");
-    return null;
-  }
+	public overrideProvider(blockChainUrl: string) {
+		this.provider = new ethers.providers.JsonRpcProvider(blockChainUrl);
+	}
 
-  public overrideProvider(blockChainUrl: string) {
-    this.provider = new ethers.providers.JsonRpcProvider(blockChainUrl);
-  }
+	public getWallet(privateKey: string) {
+		return new Wallet(privateKey, this.provider);
+	}
 
-  public getWallet(privateKey: string) {
-    return new Wallet(privateKey, this.provider);
-  }
+	public getSmartContract(
+		address: string,
+		abi: ContractInterface,
+		signer?: Signer
+	) {
+		if (signer) {
+			return new Contract(address, abi, signer);
+		}
+		return new Contract(address, abi, this.provider);
+	}
 
-  public getSmartContract(
-    address: string,
-    abi: ContractInterface,
-    signer?: Signer
-  ) {
-    if (signer) {
-      return new Contract(address, abi, signer);
-    }
-    return new Contract(address, abi, this.provider);
-  }
+	public getProviderConnection() {
+		return this.provider.connection;
+	}
 
-  public getProviderConnection() {
-    return this.provider.connection;
-  }
-
-  public async getSigner(address?: string) {
-    return await this.provider.getSigner(address);
-  }
+	public async getSigner(address?: string) {
+		return await this.provider.getSigner(address);
+	}
 }
