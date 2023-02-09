f3e43bede64e9c464135d4cffe7a3d7d2ca802ec
choo cw <chenwei.choo@ufinity.com>
2023-02-10 00:38:53 +0800

    [CCW] migrate customer component to DS

--- ether-website/src/components/customer.tsx
+++ ether-website/src/components/customer.tsx
@@ -1,8 +1,11 @@
-import { useEffect, useState } from "react";
-import { Voucher, voucherHelper } from "../ether/voucher";
+import { Card } from "@lifesg/react-design-system";
+import { Button } from "@lifesg/react-design-system/button";
 import { Masonry } from "@lifesg/react-design-system/masonry";
+import { useEffect, useState } from "react";
 import styles from "../css/customer.module.css";
 import { merchantHelper } from "../ether/merchant";
+import { Voucher, voucherHelper } from "../ether/voucher";
+import { CustomContainer } from "./common/common-styles";
 
 export interface CustomerProfile {
   walletAddress: string;
@@ -31,115 +34,108 @@ export const CustomerComponent = (): JSX.Element => {
   const getVouchers = async () => {
     const mh = merchantHelper;
 
-    const vouchers = await vh.getAllVoucher()
+    const vouchers = await vh.getAllVoucher();
 
-    return Promise.all(vouchers.map(async (voucher) => {
-      const merchant = await mh.getMerchantName(voucher.merchantAddress);
-      return {
-        ...voucher,
-        merchantName: merchant
-      };
-    }))
-
-  }
-
-  // const handleGetCustomerProfile = async (e: FormEvent<HTMLFormElement>) => {
-  // 	e.preventDefault();
-  // 	if (customerPK) {
-  // 		setGetProfile(true);
-  // 	} else {
-  // 		alert("PK Value not set");
-  // 	}
-  // };
-
-  // function handlePKChange(evt: ChangeEvent<HTMLInputElement>) {
-  // 	console.log(evt.target.value);
-  // 	setCustomerPK(evt.target.value.trim());
-  // }
-
-  // function handleChangePKBtn() {
-  // 	setCustomer(null);
-  // 	setCustomerPK("");
-  // 	setGetProfile(false);
-  // }
-  // function refreshProfile() {
-  // 	setGetProfile(true);
-  // }
+    return Promise.all(
+      vouchers.map(async (voucher) => {
+        const merchant = await mh.getMerchantName(voucher.merchantAddress);
+        return {
+          ...voucher,
+          merchantName: merchant,
+        };
+      })
+    );
+  };
 
   function handleSpendVoucher(tokenId: number, merchantAddress?: string) {
     const vh = voucherHelper;
     vh.spendVoucher(tokenId, merchantAddress);
-    setCustomer(
-      {
-        ...customer,
-        vouchers: customer.vouchers.filter(voucher => voucher.tokenId != tokenId)
-      }
-    )
+    setCustomer({
+      ...customer,
+      vouchers: customer.vouchers.filter(
+        (voucher) => voucher.tokenId != tokenId
+      ),
+    });
   }
 
   const renderVoucher = () => {
     return (
       <>
-        {customer.vouchers.map((voucher: Voucher, index: number) => {
-          return (
-            <li key={index}>
-              <div>Token ID:{voucher.tokenId.toString()}</div>
-              <div>
-                Merchant Address:
-                {vh.isEmptyHexAddress(voucher.merchantAddress.toString())
-                  ? "No merchant address"
-                  : voucher.merchantAddress.toString()}
-              </div>
-              <div>Voucher Value:{voucher.value.toString()}</div>
-            </li>
-          );
-        })}
+        {customer.vouchers.length > 0 ? (
+          customer.vouchers.map((voucher: Voucher, index: number) => {
+            return (
+              <li key={index}>
+                <Card>
+                  <div>Token ID:{voucher.tokenId.toString()}</div>
+                  <div>
+                    Merchant Address:
+                    {vh.isEmptyHexAddress(voucher.merchantAddress.toString())
+                      ? "No merchant address"
+                      : voucher.merchantAddress.toString()}
+                  </div>
+                  <div>Voucher Value:{voucher.value.toString()}</div>
+                </Card>
+              </li>
+            );
+          })
+        ) : (
+          <h3>NO VOUCHERS </h3>
+        )}
       </>
     );
   };
 
-
   const renderMerchants = () => {
     return (
-      <Masonry.Grid numOfCols={{
-        lg: 2
-      }}>
+      <Masonry.Grid
+        numOfCols={{
+          lg: 2,
+        }}
+      >
         {customer.vouchers.map((voucher: Voucher, index: number) => {
-          return <>
-            <Masonry.Tile startSm={1} colsSm={2}>
-              <div className={styles.DemoContainer}>
-                {voucher.merchantName ? voucher.merchantName : "Any Merchant"}
-                <button onClick={() => handleSpendVoucher(voucher.tokenId, !voucher.merchantName && "0x4aAED7cA70d287F2a475aB66D12bc5c0F10ceCBf")}>Spend Voucher</button>
-              </div>
-
-            </Masonry.Tile>
-          </>
+          return (
+            <>
+              <Masonry.Tile startSm={1} colsSm={2}>
+                <div className={styles.DemoContainer}>
+                  {voucher.merchantName ? voucher.merchantName : "Any Merchant"}
+                  <Button.Default
+                    onClick={() =>
+                      handleSpendVoucher(
+                        voucher.tokenId,
+                        !voucher.merchantName &&
+                          "0x4aAED7cA70d287F2a475aB66D12bc5c0F10ceCBf"
+                      )
+                    }
+                  >
+                    Spend Voucher
+                  </Button.Default>
+                </div>
+              </Masonry.Tile>
+            </>
+          );
         })}
       </Masonry.Grid>
     );
   };
 
   return (
-    <div>
-      {customer ? (
-        <div>
-          <h2>Customer Address: {customer.walletAddress}</h2>
-          <h2>ETH Balance: {customer.ETHBalance}</h2>
-          <div>
-            <h2>Vouchers:</h2>
+    <>
+      <CustomContainer>
+        {customer ? (
+          <CustomContainer>
+            <h3>Customer Address: {customer.walletAddress}</h3>
+            <h3>ETH Balance: {customer.ETHBalance}</h3>
+            <h3>Vouchers:</h3>
             <ul>{renderVoucher()}</ul>
-          </div>
-          <br />
-          {/* <button onClick={handleChangePKBtn}>Change PK</button>
-					<button onClick={refreshProfile}>Refresh Profile</button> */}
-          <>
-            <h2>Participating Merchants: </h2>
+            <br />
+            <hr />
+            <h3>Participating Merchants: </h3>
             {renderMerchants()}
-          </>
-        </div>
-      ) : (
-        <></>
-      )}
-    </div>
+          </CustomContainer>
+        ) : (
+          <></>
+        )}
+      </CustomContainer>
+    </>
   );
 };
