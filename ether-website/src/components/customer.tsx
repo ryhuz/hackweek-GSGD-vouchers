38aa133c4dd603dfe2ba6f7dbaebb8b175e709e0
choo cw <chenwei.choo@ufinity.com>
2023-02-09 20:48:21 +0800

    [CCW] retrieve voucher proper values

--- ether-website/src/components/customer.tsx
+++ ether-website/src/components/customer.tsx
@@ -1,74 +1,95 @@
 import { useEffect, useState } from "react";
-import { voucherHelper } from "../ether/voucher";
+import { Voucher, voucherHelper } from "../ether/voucher";
 
 export interface CustomerProfile {
-	walletAddress: string;
-	ETHBalance: string;
-	voucherBalance: string;
+  walletAddress: string;
+  ETHBalance: string;
+  vouchers: Voucher[];
 }
 
 export const CustomerComponent = (): JSX.Element => {
-	const [customer, setCustomer] = useState<CustomerProfile>(null);
+  const [customer, setCustomer] = useState<CustomerProfile>(null);
 
-	useEffect(() => {
-		getCustomerProfile();
-	}, []);
+  useEffect(() => {
+    getCustomerProfile();
+  }, []);
 
-	const getCustomerProfile = async () => {
-		//replace with your wallet pk
-		const ch = voucherHelper;
-		await ch.getAllVoucher();
-		const customer: CustomerProfile = {
-			walletAddress: await ch.getAddress(),
-			ETHBalance: await ch.getBalance(),
-			voucherBalance: "",
-		};
-		setCustomer(customer);
-	};
+  const getCustomerProfile = async () => {
+    //replace with your wallet pk
+    const ch = voucherHelper;
 
-	// const handleGetCustomerProfile = async (e: FormEvent<HTMLFormElement>) => {
-	// 	e.preventDefault();
-	// 	if (customerPK) {
-	// 		setGetProfile(true);
-	// 	} else {
-	// 		alert("PK Value not set");
-	// 	}
-	// };
+    const customer: CustomerProfile = {
+      walletAddress: await ch.getAddress(),
+      ETHBalance: await ch.getBalance(),
+      vouchers: await ch.getAllVoucher(),
+    };
+    console.log(customer);
+    setCustomer(customer);
+  };
 
-	// function handlePKChange(evt: ChangeEvent<HTMLInputElement>) {
-	// 	console.log(evt.target.value);
-	// 	setCustomerPK(evt.target.value.trim());
-	// }
+  // const handleGetCustomerProfile = async (e: FormEvent<HTMLFormElement>) => {
+  // 	e.preventDefault();
+  // 	if (customerPK) {
+  // 		setGetProfile(true);
+  // 	} else {
+  // 		alert("PK Value not set");
+  // 	}
+  // };
 
-	// function handleChangePKBtn() {
-	// 	setCustomer(null);
-	// 	setCustomerPK("");
-	// 	setGetProfile(false);
-	// }
-	// function refreshProfile() {
-	// 	setGetProfile(true);
-	// }
+  // function handlePKChange(evt: ChangeEvent<HTMLInputElement>) {
+  // 	console.log(evt.target.value);
+  // 	setCustomerPK(evt.target.value.trim());
+  // }
+
+  // function handleChangePKBtn() {
+  // 	setCustomer(null);
+  // 	setCustomerPK("");
+  // 	setGetProfile(false);
+  // }
+  // function refreshProfile() {
+  // 	setGetProfile(true);
+  // }
 
   function handleSpendVoucher(tokenId) {
-		const vh = voucherHelper;
+    const vh = voucherHelper;
     vh.spendVoucher(1);
   }
-	return (
-		<div>
-			{customer ? (
-				<div>
-					<h2>Customer Address: {customer.walletAddress}</h2>
-					<h2>ETH Balance: {customer.ETHBalance}</h2>
-					<h2>
-						Voucher Balance: {customer.voucherBalance?.toString()}
-					</h2>
-					{/* <button onClick={handleChangePKBtn}>Change PK</button>
+
+  const renderVoucher = () => {
+    return (
+      <>
+        {customer.vouchers.map((voucher: Voucher, index: number) => {
+          {
+            console.log("ADDRESS", voucher.merchantAddress.toString());
+          }
+          <li key={index}>
+            <div>1</div>
+            <div>{voucher.merchantAddress.toString()}</div>
+            <div>{voucher.tokenId.toString()}</div>
+            <div>{voucher.value.toString()}</div>
+          </li>;
+        })}
+      </>
+    );
+  };
+  return (
+    <div>
+      {customer ? (
+        <div>
+          <h2>Customer Address: {customer.walletAddress}</h2>
+          <h2>ETH Balance: {customer.ETHBalance}</h2>
+          <div>
+            <h2>Vouchers:</h2>
+            <ul>{renderVoucher()}</ul>
+          </div>
+          <br />
+          {/* <button onClick={handleChangePKBtn}>Change PK</button>
 					<button onClick={refreshProfile}>Refresh Profile</button> */}
           <button onClick={handleSpendVoucher}>Spend Voucher</button>
-				</div>
-			) : (
-				<></>
-			)}
-		</div>
-	);
+        </div>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
 };
--- ether-website/src/ether/voucher.ts
+++ ether-website/src/ether/voucher.ts
@@ -3,124 +3,132 @@ import { EtherHelper } from ".";
 import { currUser, _voucherContractAddress } from "../keys";
 import { _abi } from "./abi/voucher";
 
+export interface Voucher {
+  tokenId: number;
+  merchantAddress: string;
+  value: number;
+}
 class VoucherHelper {
-	private wallet: Wallet;
-	private etherHelper = new EtherHelper();
+  private wallet: Wallet;
+  private etherHelper = new EtherHelper();
 
-	// The ERC-20 Contract ABI, which is a common contract interface
-	// for tokens (this is the Human-Readable ABI format)
-	private abi = _abi;
+  // The ERC-20 Contract ABI, which is a common contract interface
+  // for tokens (this is the Human-Readable ABI format)
+  private abi = _abi;
 
-	private sc: Contract;
-	private signedSC: Contract;
-	private voucherContractAddress = _voucherContractAddress;
+  private sc: Contract;
+  private signedSC: Contract;
+  private voucherContractAddress = _voucherContractAddress;
 
-	constructor(privateKey: string) {
-		this.wallet = this.etherHelper.getWallet(privateKey);
-		this.sc = this.etherHelper.getSmartContract(
-			this.voucherContractAddress,
-			this.abi
-		);
-	}
+  constructor(privateKey: string) {
+    this.wallet = this.etherHelper.getWallet(privateKey);
+    this.sc = this.etherHelper.getSmartContract(
+      this.voucherContractAddress,
+      this.abi
+    );
+  }
 
-	public async mintVoucher(
-		address: string,
-		value: number,
-		merchantAddress?: string
-	) {
-		console.log(
-			`[mintVoucher] with params:  ${address} + ${value} + ${merchantAddress}`
-		);
-		// The provider also allows signing transactions to
-		// send ether and pay to change state within the blockchain.
-		// For this, we need the account signer...
-		try {
-			const signer = await this.etherHelper.getSigner();
-			this.signedSC = new ethers.Contract(
-				this.voucherContractAddress,
-				this.abi,
-				signer
-			);
-			const tx = merchantAddress
-				? await this.signedSC["mintVoucher(address,uint256,address)"](
-						address,
-						value,
-						merchantAddress
-				  )
-				: await this.signedSC["mintVoucher(address,uint256)"](
-						address,
-						value
-				  );
-			await tx.wait();
-		} catch (error) {
-			alert("error");
-			console.log("[ Error ]", error);
-		}
-	}
+  public async mintVoucher(
+    address: string,
+    value: number,
+    merchantAddress?: string
+  ) {
+    console.log(
+      `[mintVoucher] with params:  ${address} + ${value} + ${merchantAddress}`
+    );
+    // The provider also allows signing transactions to
+    // send ether and pay to change state within the blockchain.
+    // For this, we need the account signer...
+    try {
+      const signer = await this.etherHelper.getSigner();
+      this.signedSC = new ethers.Contract(
+        this.voucherContractAddress,
+        this.abi,
+        signer
+      );
+      const tx = merchantAddress
+        ? await this.signedSC["mintVoucher(address,uint256,address)"](
+            address,
+            value,
+            merchantAddress
+          )
+        : await this.signedSC["mintVoucher(address,uint256)"](address, value);
+      await tx.wait();
+    } catch (error) {
+      alert("error");
+      console.log("[ Error ]", error);
+    }
+  }
 
-	public async isOwner() {
-		const address = await this.wallet.getAddress();
-		console.log(`[check owner]`);
-		return (await this.sc.owner()) === address;
-	}
+  public async isOwner() {
+    const address = await this.wallet.getAddress();
+    console.log(`[check owner]`);
+    return (await this.sc.owner()) === address;
+  }
 
-	public async getBalance() {
-		try {
-			const address = await this.wallet.getAddress();
-			const etherBalance = await this.etherHelper
-				.getProvider()
-				.getBalance(address);
-			return `${ethers.utils.formatEther(etherBalance)} ETH`;
-		} catch (error) {
-			console.log(error);
-			return null;
-		}
-	}
+  public async getBalance() {
+    try {
+      const address = await this.wallet.getAddress();
+      const etherBalance = await this.etherHelper
+        .getProvider()
+        .getBalance(address);
+      return `${ethers.utils.formatEther(etherBalance)} ETH`;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
 
-	public async getAddress() {
-		return await this.wallet.getAddress();
-	}
+  public async getAddress() {
+    return await this.wallet.getAddress();
+  }
 
-	public async getAllVoucher() {
-		console.log("Getting all voucher ==========");
-		const address = await this.getAddress();
-		const result = await this.sc.connect(address).getAllVouchersMetadata();
+  public async getAllVoucher() {
+    console.log(`[getAllVoucher]`);
+    const address = await this.getAddress();
+    const results = await this.sc.connect(address).getAllVouchersMetadata();
+    console.log(`found ${results?.length} vouchers`);
+    const vouchers: Voucher[] = [];
+    for (let index = 0; index < results.length; index++) {
+      const res = results[index];
+      console.log("[voucher]", res);
+      const tokenId = ethers.BigNumber.from(res[0]).toNumber();
+      const merchantAddress = await this.sc
+        .connect(address)
+        .getVoucherMerchant(tokenId);
+      vouchers.push({
+        value: ethers.BigNumber.from(res[1]).toNumber(),
+        merchantAddress,
+        tokenId,
+      });
+    }
 
-		const ids = await this.sc.connect(address).getAllVouchersID();
-		console.log("METADATA", result);
-		console.log("IDS", ids);
-	}
+    return vouchers;
+  }
 
-	public async spendVoucher(tokenId: number, merchantAddress?: string) {
-		console.log(
-			`[spendVoucher] with params:  ${tokenId} + ${merchantAddress}`
-		);
-		try {
-			const address = await this.getAddress();
+  public async spendVoucher(tokenId: number, merchantAddress?: string) {
+    console.log(`[spendVoucher] with params:  ${tokenId} + ${merchantAddress}`);
+    try {
+      const address = await this.getAddress();
 
-			const signer = await this.etherHelper.getSigner(address);
-			this.signedSC = new ethers.Contract(
-				this.voucherContractAddress,
-				this.abi,
-				signer
-			);
+      const signer = await this.etherHelper.getSigner(address);
+      this.signedSC = new ethers.Contract(
+        this.voucherContractAddress,
+        this.abi,
+        signer
+      );
 
-			const tx = merchantAddress
-				? await this.signedSC
-						.connect(signer)
-						["spendVoucher(uint256,address)"](
-							tokenId,
-							merchantAddress
-						)
-				: await this.signedSC
-						.connect(signer)
-						["spendVoucher(uint256)"](tokenId);
-			await tx.wait();
-		} catch (error) {
-			alert("error");
-			console.log("[ Error ]", error);
-		}
-	}
+      const tx = merchantAddress
+        ? await this.signedSC
+            .connect(signer)
+            ["spendVoucher(uint256,address)"](tokenId, merchantAddress)
+        : await this.signedSC.connect(signer)["spendVoucher(uint256)"](tokenId);
+      await tx.wait();
+    } catch (error) {
+      alert("error");
+      console.log("[ Error ]", error);
+    }
+  }
 }
 
 export const voucherHelper = new VoucherHelper(currUser);
